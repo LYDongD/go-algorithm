a7b12ce4883d65879743ba18c630df0ee82748e4
lee <554050334@qq.com>
2018-12-05 09:22:28 +0800

    add new algo

---
+++ array/max-consecutive-ones_bak.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func findMaxConsecutiveOnes(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	max := 0
+	count := 0
+	for _, num := range nums {
+		if num == 1 {
+			count++
+		} else {
+			if max == 0 || count > max {
+				max = count
+			}
+			count = 0
+		}
+	}
+
+	if count > 0 && count > max {
+		max = count
+	}
+
+	return max
+}
+
+func main() {
+
+	nums1 := []int{1, 1, 0, 1, 1, 1}
+	nums2 := []int{1, 0, 1, 1, 0, 1}
+	nums3 := []int{1, 1, 1}
+	fmt.Println(findMaxConsecutiveOnes(nums1))
+	fmt.Println(findMaxConsecutiveOnes(nums2))
+	fmt.Println(findMaxConsecutiveOnes(nums3))
+
+}
