7b75eef55eadba7dbaca01d7872beb255ea8d1df
金雯 <554050334@qq.com>
2018-12-06 21:51:18 +0800

    add new algo

---
+++ array/construct-the-rectangle.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func constructRectangle(area int) []int {
+
+	if area == 0 {
+		return nil
+	}
+
+	result := make([]int, 2)
+	l := int(math.Sqrt(float64(area)))
+	w := l
+	for l*w != area {
+		if l*w > area {
+			w--
+		} else {
+			l++
+		}
+	}
+
+	result[0] = l
+	result[1] = w
+
+	return result
+
+}
+
+func main() {
+
+	fmt.Println(constructRectangle(4))
+	fmt.Println(constructRectangle(30))
+	fmt.Println(constructRectangle(10000000))
+}
