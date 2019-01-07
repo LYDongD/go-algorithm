0098a07d72f5f076ec488bcea7d68c29c34b4567
lee <554050334@qq.com>
2019-01-07 09:55:18 +0800

    addnewalgo

---
+++ array/construct-string-from-binary-tree.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+import "strconv"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func tree2str(t *TreeNode) string {
+	if t == nil {
+		return ""
+	}
+
+	currentVal := strconv.Itoa(t.Val)
+
+	if t.Left == nil && t.Right == nil {
+		return currentVal
+	}
+
+	if t.Left == nil {
+		return currentVal + "()" + "(" + tree2str(t.Right) + ")"
+	}
+
+	if t.Right == nil {
+		return currentVal + "(" + tree2str(t.Left) + ")"
+	}
+
+	return currentVal + "(" + tree2str(t.Left) + ")" + "(" + tree2str(t.Right) + ")"
+}
+
+func main() {
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 4}
+	fmt.Println(tree2str(root))
+}
