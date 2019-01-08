4bfc33c43dcfefdb42cff6b55082e375a366ae63
lee <554050334@qq.com>
2019-01-08 18:37:27 +0800

    add merge-two-binary-trees

--- tree/merge-two-binary-trees.go
+++ tree/merge-two-binary-trees.go
@@ -29,72 +29,10 @@ func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 		return t1
 	}
 
-	var result []int
+	t1.Val += t2.Val
 
-	mergeTreesRecursively(t1, t2, result)
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
 
-	fmt.Println("结果:", result)
-
-	return buildTree(result)
-
-}
-
-func mergeTreesRecursively(t1 *TreeNode, t2 *TreeNode, result []int) {
-
-	if t1 == nil && t2 == nil {
-		return
-	}
-
-	if t1 == nil {
-		result = append(result, t2.Val)
-		mergeTreesRecursively(nil, t2.Left, result)
-		mergeTreesRecursively(nil, t2.Right, result)
-	} else if t2 == nil {
-		result = append(result, t1.Val)
-		mergeTreesRecursively(t1.Left, nil, result)
-		mergeTreesRecursively(t2.Right, nil, result)
-	} else {
-		result = append(result, t1.Val+t2.Val)
-		mergeTreesRecursively(t1.Left, t2.Left, result)
-		mergeTreesRecursively(t1.Right, t2.Right, result)
-	}
-
-	fmt.Println(result)
-}
-
-func buildTree(result []int) *TreeNode {
-	index := 0
-	return buildTreePreorder(result, &index)
-}
-
-func buildTreePreorder(result []int, index *int) *TreeNode {
-	if *index >= len(result) {
-		return nil
-	}
-
-	root := &TreeNode{Val: result[*index]}
-
-	*index = *index + 1
-	root.Left = buildTreePreorder(result, index)
-	root.Right = buildTreePreorder(result, index)
-
-	return root
-
-}
-
-func preorder(root *TreeNode) {
-	if root == nil {
-		return
-	}
-
-	fmt.Print(root.Val)
-	preorder(root.Left)
-	preorder(root.Right)
-}
-
-func main() {
-
-	tree1 := []int{1, 3, 5, 2}
-	tree2 := []int{2, 1, 4, 3, 7}
-	preorder(mergeTrees(buildTree(tree1), buildTree(tree2)))
+	return t1
 }
