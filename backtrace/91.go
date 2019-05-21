6b467b1b6c67426e65e4d8bb6aba607b2afea8c6
lee <554050334@qq.com>
2019-05-21 21:05:26 +0800

    add 91

--- backtrace/91.go
+++ backtrace/91.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "strconv"
 
 func numDecodings(s string) int {
 	if len(s) == 0 {
@@ -11,28 +12,37 @@ func numDecodings(s string) int {
 		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 		'Y', 'Z'}
 
-	}
-
 	duplicatedDic := make(map[string]int)
 	return backtrace(charDic, s, "", duplicatedDic, 0)
 }
 
-
-//todo 
 func backtrace(charDic []rune, left string, selected string, duplicatedDic map[string]int, count int) int {
 	if len(left) == 0 {
+		fmt.Println("count: ", count, "selected: ", selected)
 		if duplicatedDic[selected] == 0 {
 			duplicatedDic[selected]++
 			return count + 1
+		} else {
+			return count
 		}
 	}
 
 	//select 1
-	count = backtrace(charDic, left[1:], selected+charDic[left[0]]), duplicatedDic, count)
+	first, err := strconv.Atoi(left[0:1])
+	if err == nil && first >= 1 && first <= 26 {
+		count = backtrace(charDic, left[1:], selected+string(charDic[first]), duplicatedDic, count)
+	}
 
 	//select 2
-	if len(left) >= 2 && string(left[2:]) >= "1" && string(left[2:]) <= "26" {
-		count = backtrace(charDic, left[2:], selected+charDic[left[0]]+charDic[left[1]], duplicatedDic, count)
+	if len(left) >= 2 && left[0:1] != "0" {
+		double, err1 := strconv.Atoi(left[0:2])
+		if double >= 1 && double <= 26 {
+			fmt.Println("select 2", selected+string(charDic[double]))
+			if err1 == nil {
+				count = backtrace(charDic, left[2:], selected+string(charDic[double]), duplicatedDic, count)
+			}
+
+		}
 	}
 
 	return count
@@ -40,4 +50,7 @@ func backtrace(charDic []rune, left string, selected string, duplicatedDic map[s
 
 func main() {
 	fmt.Println(numDecodings("226"))
+	fmt.Println(numDecodings("12"))
+	fmt.Println(numDecodings("0"))
+	fmt.Println(numDecodings("01"))
 }
