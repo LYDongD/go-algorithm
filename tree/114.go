cf2da676cb7261d950c2e58ced6591527cb2d3ba
lee <554050334@qq.com>
2019-06-13 09:45:53 +0800

    formate change in 114

--- tree/114.go
+++ tree/114.go
@@ -3,45 +3,45 @@ package main
 import "fmt"
 
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-func flatten(root *TreeNode)  {
-    if root == nil {
-	return
-    }
+func flatten(root *TreeNode) {
+	if root == nil {
+		return
+	}
 
-    flatternRecursively(root)
+	flatternRecursively(root)
 }
 
 func flatternRecursively(node *TreeNode) *TreeNode {
-    if node.Left == nil && node.Right == nil {
-	return node
-    }
+	if node.Left == nil && node.Right == nil {
+		return node
+	}
 
-    var tail *TreeNode
-    if node.Left != nil {
-	tail = flatternRecursively(node.Left)
-    }
+	var tail *TreeNode
+	if node.Left != nil {
+		tail = flatternRecursively(node.Left)
+	}
 
-    if tail != nil {
-	tail.Right = node.Right
-    }
+	if tail != nil {
+		tail.Right = node.Right
+	}
 
-    if node.Right != nil {
-	tail = flatternRecursively(node.Right)
-    }
+	if node.Right != nil {
+		tail = flatternRecursively(node.Right)
+	}
 
-     if node.Left != nil {
-        node.Right = node.Left
-        node.Left = nil
-    }
+	if node.Left != nil {
+		node.Right = node.Left
+		node.Left = nil
+	}
 
-    return tail
+	return tail
 }
 
 func main() {
-    fmt.Println("haha")
+	fmt.Println("haha")
 }
