b928ea1c64b0c31e0b0053d0f3fc7a8d7cb266d8
liam <554050334@qq.com>
2019-07-20 11:13:54 +0800

    add algo 147

--- link/147.go
+++ link/147.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math"
 
 type ListNode struct {
     Val int
@@ -12,29 +13,55 @@ func insertionSortList(head *ListNode) *ListNode {
 	return head
     }
 
-    dummy := &ListNode{Next:head}
-    prevCursor, cursor := head, head.Next
-    for (cursor != nil) {
-	nextCursor := cursor.Next
-	prevNode, node := dummy, dummy.Next
-	for node != cursor {
-	    if node.Val > cursor.Val {
-		prevCursor.Next = nextCursor
-		cursor.Next = node
-		prevNode.Next = cursor
-		break
-	    }
-
-	    prevNode = node
-	    node = node.Next
+    //create the dummy with minest val
+    dummy := &ListNode{Val:int(math.MinInt64), Next:head}
+    for cursor := dummy; cursor.Next != nil; {
+	//optimize
+	if cursor.Val <= cursor.Next.Val {
+	    cursor = cursor.Next
+	    continue
 	}
 
-	cursor = nextCursor
+	//compare insert
+	for orderedNode := dummy; orderedNode != cursor; orderedNode = orderedNode.Next {
+	    //compare next val and that will reduce one pointer
+	    if orderedNode.Next.Val > cursor.Next.Val {
+		nextOrderedNode := orderedNode.Next
+		orderedNode.Next = cursor.Next
+		cursor.Next = cursor.Next.Next
+		orderedNode.Next.Next = nextOrderedNode
+		break
+	    }
+	}
     }
 
     return dummy.Next
 }
 
+func makeList(nums []int) *ListNode {
+    head := &ListNode{Val : nums[0]}
+    cursor := head
+    for i := 1; i< len(nums); i++  {
+	cursor.Next = &ListNode{Val: nums[i]}
+	cursor = cursor.Next
+    }
+    return head
+}
+
+func printListNode(head *ListNode) {
+    cursor := head
+    for cursor != nil {
+	fmt.Print(cursor.Val)
+	if cursor.Next != nil {
+	    fmt.Print("->")
+	}
+	cursor = cursor.Next
+    }
+    fmt.Println("")
+}
+
 func main() {
-    fmt.Println("hah")
+    head := makeList([]int{-1,5,3,4,0})
+    node := insertionSortList(head)
+    printListNode(node)
 }
