43b25cf7c24bc55c85d29a209acd0cb1214fb85a
lee <554050334@qq.com>
2019-09-06 10:57:21 +0800

    add 227

--- strings/227.go
+++ strings/227.go
@@ -5,78 +5,131 @@ import "strings"
 import "strconv"
 
 type Stack struct {
-    elements []byte
+	elements []string
 }
 
 func (self *Stack) IsEmpty() bool {
-    return len(self.elements) == 0
+	return len(self.elements) == 0
 }
 
-func (self *Stack) Push(ele byte) {
-    self.elements = append(self.elements, ele)
+func (self *Stack) Push(ele string) {
+	self.elements = append(self.elements, ele)
 }
 
-func (self *Stack( Pop() byte {
-    top := self.elements[len(self.elements) - 1]
-    self.elements = self.elements[0, len(self.elements-1]
-    return top
+func (self *Stack) Pop() string {
+	top := self.elements[len(self.elements)-1]
+	self.elements = self.elements[0 : len(self.elements)-1]
+	return top
+}
+
+func (self *Stack) reverse() *Stack {
+	newStack := &Stack{[]string{}}
+	for !self.IsEmpty() {
+		newStack.Push(self.Pop())
+	}
+
+	return newStack
 }
 
 func calculate(s string) int {
-    if len(s) == 0 {
-	return 0
-    }
-
-    s = strings.Replace(s, " ", "", -1)
-    if len(s) == 0 {
-	return 0
-    }
-
-    stack := &Stack{[]int{}}
-    i := 0
-    for i <= len(s) - 1 {
-	if isMultiOperator(s[i]) {
-	    result := doOperator(stack.Pop(), s[i], s[i+1])
-	    i += 2
-	    stack.Push(result)
-	}else {
-	    stack.Push(s[i])
-	    i++
-	}
-    }
-
-    for !stack.IsEmpty() {
-	b := stack.Pop()
-	if stack.IsEmpty() {
-	    return b
+	if len(s) == 0 {
+		return 0
 	}
 
-	operator := stack.Pop()
-	a := stack.Pop()
-	stack.Push(doOperator(a, operator, b)
-    }
+	s = strings.Replace(s, " ", "", -1)
+	if len(s) == 0 {
+		return 0
+	}
 
-    return 0
+	stack := &Stack{[]string{}}
+	i := 0
+	for i <= len(s)-1 {
+		if isMultiOperator(s[i : i+1]) {
+			num, next := getNextNum(s, i+1)
+			result := doOperator(stack.Pop(), s[i:i+1], num)
+			i = next
+			stack.Push(result)
+		} else if isAddOperator(s[i : i+1]) {
+			stack.Push((s[i : i+1]))
+			i++
+		} else {
+			num, next := getNextNum(s, i)
+			stack.Push(num)
+			i = next
+		}
+	}
+
+	//倒转栈
+	stack = stack.reverse()
+
+	for !stack.IsEmpty() {
+		a := stack.Pop()
+		if stack.IsEmpty() {
+			result, err := strconv.Atoi(a)
+			if err == nil {
+				return result
+			}
+		}
+
+		operator := stack.Pop()
+		b := stack.Pop()
+		stack.Push(doOperator(a, operator, b))
+	}
+
+	return 0
 
 }
 
-func isMultiOperator(char byte) bool {
-    return char == '*' || char == '/'
+func isMultiOperator(char string) bool {
+	return char == "*" || char == "/"
 }
 
-func doOperator(a, operator, b byte) byte{
-    result := 0
-    aint := a - '0'
-    bint := b - '0'
-    if operator == '+' {
-	result = aint + bint
-    }else if operator == '-' {
-	result = aint - bint
-    }else if operator ==  '*' {
-	result = aint * bint
-    }else {
-	reuslt = aint / bint
-    }
+func isAddOperator(char string) bool {
+	return char == "+" || char == "-"
+}
 
-    return strconv.Itoa(result)[0]
+func getNextNum(s string, start int) (string, int) {
+	num := ""
+	i := 0
+	for i = start; i < len(s); i++ {
+		if isMultiOperator(s[i:i+1]) || isAddOperator(s[i:i+1]) {
+			break
+		}
+		num = num + s[i:i+1]
+	}
+
+	return num, i
+}
+
+func doOperator(a, operator, b string) string {
+	result := 0
+	aint, err := strconv.Atoi(a)
+	if err != nil {
+		panic("a is not a num")
+	}
+	bint, err := strconv.Atoi(b)
+	if err != nil {
+		panic("b is not a num")
+	}
+	if operator == "+" {
+		result = aint + bint
+	} else if operator == "-" {
+		result = aint - bint
+	} else if operator == "*" {
+		result = aint * bint
+	} else {
+		result = aint / bint
+	}
+
+	return strconv.Itoa(result)
+}
+
+func main() {
+	fmt.Println(calculate("42"))
+	fmt.Println(calculate("1-1+1"))
+	fmt.Println(calculate("3+2*2"))
+	fmt.Println(calculate(" 3/2 "))
+	fmt.Println(calculate(" 3+5 / 2 "))
+	fmt.Println(calculate("0-2147483647"))
+
 }
